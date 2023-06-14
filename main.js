9f2b84a50506b4f6e9d2f8305b8ec1b564cd7c09
JDev000 <124886417+JDev000@users.noreply.github.com>
2023-06-14 22:25:24 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// const firstCard = document.querySelector('.first-card')
+// const displayTxtCard = document.getElementById('display-txt-card');
+
+// firstCard.addEventListener('mouseover', ()=>{
+//     displayTxtCard.style.color = 'white';
+// })
